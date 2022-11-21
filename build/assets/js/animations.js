95288b51f4f2133701c458de43c4c43c8e912f42
RusudanNovikova <rusudan0@gmail.com>
2022-11-21 16:25:13 +0300

    Start

---
+++ build/assets/js/animations.js
@@ -0,0 +1,184 @@
+// headlines animation
+function animateHeadlines(splits) {
+  splits.forEach((spl) => {
+    if (spl.anim) {
+      spl.anim.progress(1).kill();
+    }
+
+    Splitting({
+      target: spl,
+      by: "chars",
+      key: null,
+      whitespace: true,
+    });
+
+    var chars = spl.querySelectorAll(".char");
+    spl.anim = gsap.to(chars, {
+      scrollTrigger: {
+        trigger: spl,
+        toggleActions: "restart pause resume reverse",
+        start: "top 85%",
+      },
+      duration: 0.2,
+      ease: "linear",
+      y: "-0.1em",
+      opacity: 1,
+      stagger: 0.01,
+    });
+  });
+  splitsSet = true;
+  splits.forEach((spl) => {
+    spl.style.visibility = "visible";
+  });
+}
+
+var splits = document.querySelectorAll(".split");
+animateHeadlines(splits);
+
+var cardEffect3Wrappers = document.querySelectorAll(".card-effect-03-wrapper");
+
+cardEffect3Wrappers.forEach((wrap) => {
+  wrap.onmousemove = (e) => {
+    for (const card of wrap.querySelectorAll(".card-effect-03")) {
+      const rect = card.getBoundingClientRect(),
+        x = e.clientX - rect.left,
+        y = e.clientY - rect.top;
+
+      card.style.setProperty("--mouse-x", `${x}px`);
+      card.style.setProperty("--mouse-y", `${y}px`);
+    }
+  };
+});
+
+ScrollTrigger.create({
+  trigger: ".block-why-rocks",
+  start: "top top",
+  end: "bottom bottom",
+  toggleClass: "pinned",
+  onEnter: function () {
+    document.querySelector(".block-why-rocks").classList.add("pinned-bottom");
+    document.querySelector(".block-why-rocks").classList.remove("pinned-top");
+  },
+  onEnterBack: function () {
+    document.querySelector(".block-why-rocks").classList.remove("pinned-bottom");
+    document.querySelector(".block-why-rocks").classList.add("pinned-top");
+  },
+  onLeave: function () {
+    document.querySelector(".block-why-rocks").classList.add("pinned-bottom");
+    document.querySelector(".block-why-rocks").classList.remove("pinned-top");
+  },
+  onLeaveBack: function () {
+    document.querySelector(".block-why-rocks").classList.remove("pinned-bottom");
+    document.querySelector(".block-why-rocks").classList.add("pinned-top");
+  },
+});
+ScrollTrigger.create({
+  trigger: ".block-why-rocks",
+  start: "top top",
+  end: "bottom bottom",
+  pin: ".block-why-rocks .text",
+});
+
+var listItems = document.querySelectorAll(".block-why-rocks .scroller li");
+
+gsap.utils.toArray(listItems).forEach((li, i) => {
+  gsap.to(li.querySelector(".txt-lg"), {
+    keyframes: {
+      scale: [1, 1, 1, 1, 1, 1, 1, 1.1, 1.2, 2, 3, 4, 5, 6, 7, 7, 7, 7, 7],
+      opacity: [0, 0.1, 0.2, 0.3, 0.5, 1, 0.6, 0.4, 0, 0, 0, 0, 0, 0, 0],
+    },
+    transformOrigin: "center",
+    ease: "none",
+    pin: li,
+    pinSpacing: false,
+    scrollTrigger: {
+      trigger: li,
+      start: "top bottom",
+      end: "bottom top",
+      scrub: 1,
+      snap: {
+        snapTo: li,
+        // inertia: false,
+        duration: 1,
+        delay: 0,
+      },
+    },
+  });
+
+  gsap.to(li.querySelector("p"), {
+    keyframes: {
+      opacity: [0, 0.5, 1, 0.8, 0.4, 0.3, 0, 0, 0, 0],
+    },
+    transformOrigin: "center",
+    ease: "none",
+    pin: li,
+    pinSpacing: false,
+    scrollTrigger: {
+      trigger: li,
+      start: "top bottom",
+      end: "bottom top",
+      scrub: 1,
+      snap: {
+        snapTo: li,
+        // inertia: false,
+        duration: 0.3,
+        delay: 0,
+      },
+    },
+  });
+});
+
+var lines = document.querySelectorAll(".line-animated");
+gsap.utils.toArray(lines).forEach((line, i) => {
+  gsap.to(line, {
+    keyframes: {
+      scaleX: [0, 1],
+    },
+    transformOrigin: "center",
+    ease: "none",
+    scrollTrigger: {
+      trigger: line,
+      start: "top 80%",
+      duration: 0.2,
+      toggleActions: "restart pause resume reverse",
+    },
+  });
+});
+
+window.addEventListener("load", function () {
+  ScrollTrigger.refresh();
+});
+
+// footer title
+function getMousePos(evt) {
+  var doc = document.documentElement || document.body;
+  var pos = {
+    x: evt ? evt.pageX : window.event.clientX + doc.scrollLeft - doc.clientLeft,
+  };
+  console.log(pos.x);
+  return pos;
+}
+
+document.querySelector(".footer-title").onmousemove = moveMouse;
+document.querySelector(".footer-title").onmouseleave = function () {
+  this.removeAttribute("style");
+};
+
+function moveMouse(evt) {
+  var pos = getMousePos(evt),
+    followMouse = document.querySelector(".footer-title");
+  followMouseX = followMouse.clientWidth / 2;
+  followMouse.style.backgroundPosition = pos.x - followMouseX + "px " + "0px";
+}
+
+// image overlay
+var overlayContainer = document.querySelector(".block-hello .image");
+var overlay = document.querySelector(".block-hello .image .overlay");
+overlayContainer.onmousemove = (e) => {
+  const rect = overlayContainer.getBoundingClientRect(),
+    x = e.clientX - rect.left,
+    y = e.clientY - rect.top;
+
+  overlay.style.setProperty("--mouse-x", `${x}px`);
+  overlay.style.setProperty("--mouse-y", `${y}px`);
+};
---
+++ gulp/tasks/stylesmin.js
@@ -0,0 +1,39 @@
+const gulp = require("gulp");
+const plumber = require("gulp-plumber");
+const scss = require("gulp-sass");
+const cleanCSS = require("gulp-clean-css");
+const autoprefixer = require("gulp-autoprefixer");
+const argv = require("yargs").argv;
+const gulpif = require("gulp-if");
+var rename = require("gulp-rename");
+
+module.exports = function stylesmin() {
+  return (
+    gulp
+      .src("src/scss/*.scss")
+      .pipe(plumber())
+      .pipe(
+        autoprefixer({
+          overrideBrowserslist: ["last 4 version"],
+          cascade: false,
+        })
+      )
+      // .pipe(rename({ suffix: ".min" }))
+      .pipe(scss({ outputStyle: "compressed" }))
+      .pipe(
+        gulpif(
+          argv.prod,
+          cleanCSS(
+            {
+              debug: true,
+              compatibility: "*",
+            },
+            (details) => {
+              console.log(`${details.name}: Original size:${details.stats.originalSize} - Minified size: ${details.stats.minifiedSize}`);
+            }
+          )
+        )
+      )
+      .pipe(gulp.dest("build/assets/css"))
+  );
+};
