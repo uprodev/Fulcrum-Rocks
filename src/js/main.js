57b6355002176ce4f4ecaad8fa5d5c11bfd910cc
RusudanNovikova <rusudan0@gmail.com>
2022-11-25 12:15:59 +0300

    changes 25.11-12:15

--- src/js/main.js
+++ src/js/main.js
@@ -166,35 +166,48 @@ jQuery(document).ready(function ($) {
       $(this).parent().removeClass("error");
     }
   });
+
+  $(".card-effect-05")
+    .on("mouseenter", function () {
+      var color = $(this).attr("data-color");
+      $(this)
+        .parents(".block-stages")
+        .addClass("color-" + color);
+    })
+    .on("mouseleave", function () {
+      var color = $(this).attr("data-color");
+      $(this)
+        .parents(".block-stages")
+        .removeClass("color-" + color);
+    });
 });
 
-var observe;
-if (window.attachEvent) {
-  observe = function (element, event, handler) {
-    element.attachEvent("on" + event, handler);
-  };
-} else {
-  observe = function (element, event, handler) {
-    element.addEventListener(event, handler, false);
-  };
-}
-function initTextarea() {
-  var text = document.getElementById("contactFormMessage");
-  function resize() {
-    text.style.height = "auto";
-    text.style.height = text.scrollHeight + "px";
+if (document.getElementById("contactFormMessage")) {
+  var observe;
+  if (window.attachEvent) {
+    observe = function (element, event, handler) {
+      element.attachEvent("on" + event, handler);
+    };
+  } else {
+    observe = function (element, event, handler) {
+      element.addEventListener(event, handler, false);
+    };
   }
-  /* 0-timeout to get the already changed text */
-  function delayedResize() {
-    window.setTimeout(resize, 0);
+  function initTextarea() {
+    var text = document.getElementById("contactFormMessage");
+    function resize() {
+      text.style.height = "auto";
+      text.style.height = text.scrollHeight + "px";
+    }
+    function delayedResize() {
+      window.setTimeout(resize, 0);
+    }
+    observe(text, "change", resize);
+    observe(text, "cut", delayedResize);
+    observe(text, "paste", delayedResize);
+    observe(text, "drop", delayedResize);
+    observe(text, "keydown", delayedResize);
+    resize();
   }
-  observe(text, "change", resize);
-  observe(text, "cut", delayedResize);
-  observe(text, "paste", delayedResize);
-  observe(text, "drop", delayedResize);
-  observe(text, "keydown", delayedResize);
-  // text.focus();
-  // text.select();
-  resize();
+  initTextarea();
 }
-initTextarea();
